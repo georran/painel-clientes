d3aa52bfce971f8f6d8ff271b3ab2c36b5987b01
Georran <georran@github.com>
2022-08-05 18:17:57 -0300

    Functions Arrumadas

---
+++ src/cliente/adicionarContatos.ts
@@ -0,0 +1,44 @@
+import { CreateClienteDto } from './dto/create-cliente.dto';
+import { Cliente } from './entities/cliente.entity';
+
+export function adicionarContatos(clientes: CreateClienteDto) {
+  const cliente = new Cliente();
+  cliente.empresa = clientes.empresa;
+  cliente.mensalidade = clientes.mensalidade;
+  cliente.versao = clientes.versao;
+  cliente.dataImplatancao = clientes.dataImplatancao;
+  cliente.numero = adicionarNums(clientes, cliente.id);
+  cliente.whatsapp = tipoNum(clientes)[0];
+  cliente.telefone = tipoNum(clientes)[1];
+  return cliente;
+}
+function adicionarNums(numero: CreateClienteDto, repositorio) {
+  const num = new Cliente();
+  num.numero = [null];
+  numero.idnum.forEach((numeros, i) => {
+    num.numero[i] = {
+      identificador: numeros,
+      cliente: repositorio,
+      ntipo: numero.tipo[i],
+      id: num.id,
+    };
+    console.log('adicionados:', numeros, numero.tipo[i]);
+  });
+  return num.numero;
+}
+function tipoNum(tipo: CreateClienteDto) {
+  const cliente = new Cliente();
+  if (
+    (tipo.tipo.includes('WhatsApp', 0) && tipo.tipo.includes('Telefone', 0)) ||
+    tipo.tipo.includes('Hibrido', 0)
+    ) {
+    cliente.whatsapp = cliente.telefone = true;
+  console.log('Tem Ambos');
+} else if (tipo.tipo.includes('Telefone', 0)) {
+  cliente.whatsapp = false;
+  cliente.telefone = true;
+  console.log('Tem somente Telefone');
+}
+const tipos = [cliente.whatsapp, cliente.telefone];
+return tipos;
+}
--- src/cliente/cliente.service.ts
+++ src/cliente/cliente.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { adicionarContatos } from './adicionarContatos';
 import { CreateClienteDto } from './dto/create-cliente.dto';
 import { UpdateClienteDto } from './dto/update-cliente.dto';
 import { Cliente } from './entities/cliente.entity';
@@ -12,49 +13,7 @@ export class ClienteService {
     private clientesRepository: Repository<Cliente>,
   ) {}
   create(createClienteDto: CreateClienteDto) {
-    const cliente = new Cliente();
-    cliente.empresa = createClienteDto.empresa;
-    cliente.mensalidade = createClienteDto.mensalidade;
-    cliente.versao = createClienteDto.versao;
-    cliente.dataImplatancao = createClienteDto.dataImplatancao;
-    if (
-      (createClienteDto.tipo.includes('WhatsApp', 0) &&
-        createClienteDto.tipo.includes('Telefone', 0)) ||
-      createClienteDto.tipo.includes('Hibrido', 0)
-    ) {
-      cliente.whatsapp = cliente.telefone = true;
-      console.log('Tem Ambos');
-    } else if (createClienteDto.tipo.includes('Telefone', 0)) {
-      cliente.whatsapp = false;
-      cliente.telefone = true;
-      console.log('Tem somente Telefone');
-    }
-    // forEach
-    cliente.numero = [null];
-    createClienteDto.idnum.forEach((numeros, i) => {
-      cliente.numero[i] = {
-        identificador: numeros,
-        cliente: this.clientesRepository.getId(cliente),
-        ntipo: createClienteDto.tipo[i],
-        id: cliente.id,
-      };
-      console.log('adicionados:', numeros, createClienteDto.tipo[i]);
-    });
-    // for
-    /* for (let n = 0; n < createClienteDto.idnum.length; n++) {
-      cliente.numero[n] = {
-        identificador: createClienteDto.idnum[n],
-        cliente: this.clientesRepository.getId(cliente),
-        ntipo: createClienteDto.tipo[n],
-        id: cliente.id,
-      };
-      console.log(
-        'adicionados:',
-        createClienteDto.idnum[n],
-        createClienteDto.tipo[n],
-      );
-    } */
-    return this.clientesRepository.save(cliente);
+    return this.clientesRepository.save(adicionarContatos(createClienteDto));
   }
 
   async findAll() {
